e84deb899da173a9521d5ba93b3303b8ab748440
ramji-gif <sssrt943@gmail.com>
2025-05-21 03:26:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -216,4 +216,4 @@ async def startup_event():
 # -----------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
